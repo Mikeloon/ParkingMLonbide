513889da2b38d6d68f724ebc9284e6836277ffb4
Mikel Lonbide <mikellonbide@gmail.com>
2023-06-18 01:41:03 +0200

    Primeras modificaciones añadiendo firebase, Firebase authentication

--- app/src/main/java/com/lksnext/parkingmlonbide/RegisterLogin/MainActivity.java
+++ app/src/main/java/com/lksnext/parkingmlonbide/RegisterLogin/MainActivity.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.lksnext.parkingmlonbide.DataClasses.User;
 import com.lksnext.parkingmlonbide.NavFragments.HomePage;
 import com.lksnext.parkingmlonbide.R;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,26 +26,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+        TextView emailEditText = (TextView) findViewById(R.id.username);
+        TextView passwordEditText = (TextView) findViewById(R.id.password);
+        TextView returnRegister = (TextView) findViewById(R.id.goRegister);
 
         MaterialButton loginbton = (MaterialButton) findViewById(R.id.loginbtn);
 
         loginbton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals(User.username) && password.getText().toString().equals(User.password)){
-                    //correct
-                    Toast.makeText(MainActivity.this, "Sesion Iniciada", Toast.LENGTH_SHORT).show();
-                    openHomepage();
-                }else
-                    //incorrect
-                    Toast.makeText(MainActivity.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                       emailEditText.getText().toString(),
+                        passwordEditText.getText().toString()).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()){
+                                openHomepage();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Error al iniciar sesion", Toast.LENGTH_SHORT).show();
+                            }
+                });
             }
 
         });
-    }
 
+        returnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToRegister = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(goToRegister);
+            }
+        });
+    }
     public void openHomepage(){
         Intent goToHome = new Intent(MainActivity.this, HomePage.class);
         startActivity(goToHome);
--- app/src/main/java/com/lksnext/parkingmlonbide/RegisterLogin/RegisterActivity.java
+++ app/src/main/java/com/lksnext/parkingmlonbide/RegisterLogin/RegisterActivity.java
@@ -7,20 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.lksnext.parkingmlonbide.DataClasses.User;
 import com.lksnext.parkingmlonbide.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         TextView username = (TextView) findViewById(R.id.usernameR);
-        TextView birth = (TextView) findViewById(R.id.fechanac);
         TextView email = (TextView) findViewById(R.id.email);
         TextView pass = (TextView) findViewById(R.id.passwordR);
         TextView repPass = (TextView) findViewById(R.id.repetirpass);
@@ -29,17 +31,22 @@ public class RegisterActivity extends AppCompatActivity {
         registerbton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (pass.getText().toString().equals(repPass.getText().toString())){
-                    User.username = username.getText().toString();
-                    User.password = pass.getText().toString();
-                    User.birthdate = birth.getText().toString();
-                    User.email = email.getText().toString();
-                    Intent goToLogin = new Intent(RegisterActivity.this,MainActivity.class);
-                    Toast.makeText(RegisterActivity.this, "Registro completado", Toast.LENGTH_SHORT).show();
-                    startActivity(goToLogin);
+                if (pass.getText().toString().equals(repPass.getText().toString()) && pass.getText().toString().length() > 5){
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                            email.getText().toString(),
+                            pass.getText().toString()
+                    ).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()){
+                            Intent goToLogin = new Intent(RegisterActivity.this,MainActivity.class);
+                            Toast.makeText(RegisterActivity.this, "Registro completado", Toast.LENGTH_SHORT).show();
+                            startActivity(goToLogin);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error al registrarse", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Error al registrarse", Toast.LENGTH_SHORT).show();
                 }
-                else Toast.makeText(RegisterActivity.this, "Error al registrarse", Toast.LENGTH_SHORT).show();
-
             }
         });
     }
